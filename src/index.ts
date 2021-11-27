9740d45b81129145ed847a9d3f2a65f27be9d260
Marek Nowosiadły <nowosiadlymarek@gmail.com>
2021-11-27 19:04:32 +0100

    refactor: use module.exports in index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { injectionToken } from './rules/injection-token';
 
-export const rules = {
-  'injection-token': injectionToken,
+module.exports = {
+  rules: {
+    'injection-token': injectionToken,
+  },
 };
