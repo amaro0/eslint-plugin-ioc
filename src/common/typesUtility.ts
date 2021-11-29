a2386d8fa773654ea94cdb28e16fde1d35731343
Marek Nowosiadły <nowosiadlymarek@gmail.com>
2021-11-29 20:26:59 +0100

    chore: move ts to peer dependencies

--- src/common/typesUtility.ts
+++ src/common/typesUtility.ts
@@ -2,7 +2,7 @@ import { ESLintUtils, TSESLint } from '@typescript-eslint/experimental-utils';
 import { TSESTree } from '@typescript-eslint/typescript-estree/dist/ts-estree';
 import { Type } from 'typescript';
 
-export function getType(context: TSESLint.RuleContext<string, unknown[]>, node: TSESTree.Node): Type{
+export function getType(context: TSESLint.RuleContext<string, unknown[]>, node: TSESTree.Node): Type {
   const parserServices = ESLintUtils.getParserServices(context);
   const checker = parserServices.program.getTypeChecker();
 
